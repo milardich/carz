7b0f106b5db793e768d8077c1b5cfc3eaff33ece
milardich <stjepan.milardic1@gmail.com>
2022-12-02 01:55:37 +0100

    item layout

--- index.php
+++ index.php
@@ -88,10 +88,43 @@
             </div>
         </div>
 
-        <div class="items-container">
-            items
+        <div class="items-container row">
+            <?php
+                include("item.php");
+                include("item.php");
+                include("item.php");
+                include("item.php");
+                include("item.php");
+                include("item.php");
+            ?>
         </div>
     </div>
+
+    <footer class="bg-dark text-center text-white">
+        <div class="container p-4">
+            <section class="mb-4">
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                ><i class="fab fa-linkedin-in"></i
+                    ></a>
+
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                ><i class="fab fa-github"></i
+                    ></a>
+            </section>
+
+            <section class="mb-4">
+                <p>
+                    Projekt iz web programiranja
+                </p>
+            </section>
+        </div>
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2022 Copyright:
+            <a class="text-white" href="#">Stjepan Milardic</a>
+        </div>
+
+    </footer>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>

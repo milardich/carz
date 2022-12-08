f607eec0ae2e0d40a38a040e1b31749730d492a0
milardich <stjepan.milardic1@gmail.com>
2022-12-08 21:56:51 +0100

    image name randomisation

--- classes/imageHandler.php
+++ classes/imageHandler.php
@@ -5,25 +5,12 @@ include_once 'database.php';
 class ImageHandler{
 
     public function UploadImage($img){
-        
-        $rand_prefix = $this->generateRandomString();
-
-        if(move_uploaded_file($img["tmp_name"], "images/" . $rand_prefix .  $img["name"])){
+        if(move_uploaded_file($img["tmp_name"], "images/" .  $img["name"])){
             echo "\n";
             echo "Image uploaded: " . $img["name"] . " [images/".$img["name"]; 
         }
         else{
             echo "Error uploading img";
         }
-    }
-
-    private function generateRandomString($length = 15){
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_';
-        $charactersLength = strlen($characters);
-        $randomString = '';
-        for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[rand(0, $charactersLength - 1)];
-        }
-        return $randomString;
     }
 }
--- create_item_script.php
+++ create_item_script.php
@@ -18,13 +18,24 @@ else{
     $itemHandler->SetItemDescription($_POST["itemDescription"]);
     $itemHandler->SetItemLocation($_POST["itemLocation"]);
     $itemHandler->SetItemPrice($_POST["itemPrice"]);
+    $img_name = $_FILES["thumbnail_img"]["name"];
+    $_FILES["thumbnail_img"]["name"] = generateRandomString() . $img_name;
     $itemHandler->SetItemThumbnail("images/" . $_FILES["thumbnail_img"]["name"]);
-    //$itemHandler->SaveItem();
+    $itemHandler->SaveItem();
 
     $imageHandler = new ImageHandler();
     $imageHandler->UploadImage($_FILES["thumbnail_img"]);
 
     echo "\n Saved item: \n";
-    echo $_POST["itemTitle"] . ": " . $_POST["itemPrice"];
-    
+    echo $_POST["itemTitle"] . ": " . $_POST["itemPrice"];   
+}
+
+function generateRandomString($length = 15){
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
 }

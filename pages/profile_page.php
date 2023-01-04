1625a6dccffc6175a2a71c947c5044c758f5c4e8
milardich <stjepan.milardic1@gmail.com>
2023-01-04 16:26:49 +0100

    displaying items on user profile

--- pages/profile_page.php
+++ pages/profile_page.php
@@ -13,13 +13,13 @@
     ?>
     
     <div class="container">
-        <div class="user-information-container row m-2">
+        <div class="user-information-container row m-2 p-2">
             <?php
                 $userHandler = new User();
                 $userData = $userHandler->GetUserDataById($_SESSION["LOGGED_IN_USER_ID"]);
             ?>
             <div class="user-profile-picture col-lg-4 col-sm-12 m-auto">
-                <img src="../resources/random_guy.jpg" alt="">
+                <img src=<?php echo "../" . $userData["profile_picture_url"]; ?> alt="">
             </div>
             <div class="col-lg-8 col-sm-12">
                 <div class="user-info-text h3 mt-3">
@@ -38,12 +38,49 @@
             My items:
         </div>
 
-        <div class="items-container">
+        <div class="items-container row">
             <?php
                 $itemHandler = new ItemHandler();
                 $items = $itemHandler->GetItemsFromUser($_SESSION["LOGGED_IN_USER_ID"]);
                 foreach($items as $item){
-                    echo $item["item_title"] . "<br>";
+                    ?>
+                    <div class="col-md-3 item-container">
+                        <a href="../pages/item_page.php?id=<?php echo $item["item_id"]; ?>">
+                            <div class="item">
+                                <div class="item-thumbnail-container">
+                                    <img src="../<?php echo $item["item_thumbnail"]; ?>">
+                                </div>
+                                <div class="item-title">
+                                    <?php echo $item["item_title"]; ?>
+                                </div>
+                                <div class="item-description">
+                                    <?php echo $item["item_description"]; ?>
+                                </div>
+                                <div class="date-and-price-container">
+                                    <div class="row">
+                                        <div class="col-md-6 d-flex">
+                                            <div class="d-block mt-auto">
+                                                <object data="../resources/location-pin-solid.svg" width="15" height="15"> </object>
+                                                <span class="location-text"><?php echo $item["item_location"]; ?></span>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="text-end">
+                                                <div class="price-1">
+                                                    $<?php echo $item["item_price"]; ?>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="save-button-container" id="<?php echo $item["item_id"]; ?>">
+                                <object data="../resources/bookmark-solid.svg" width="30" height="30"> </object>
+                            </div>
+                        </a>
+                    </div>
+                    <?php
                 }
             ?>
         </div>

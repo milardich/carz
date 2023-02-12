a24e4e2f29cb30b8abf102ece7752dd972f2e1d5
milardich <stjepan.milardic1@gmail.com>
2023-02-12 23:58:31 +0100

    update sql

--- resources/carz.sql
+++ resources/carz.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 01, 2023 at 05:50 PM
+-- Generation Time: Feb 12, 2023 at 11:57 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -29,16 +29,18 @@ USE `carz`;
 -- Table structure for table `car_makers`
 --
 
-CREATE TABLE `car_makers` (
-  `car_maker_id` int(11) NOT NULL,
-  `car_maker_name` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `car_makers`;
+CREATE TABLE IF NOT EXISTS `car_makers` (
+  `car_maker_id` int(11) NOT NULL AUTO_INCREMENT,
+  `car_maker_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`car_maker_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `car_makers`
 --
 
-INSERT INTO `car_makers` (`car_maker_id`, `car_maker_name`) VALUES
+INSERT DELAYED IGNORE INTO `car_makers` (`car_maker_id`, `car_maker_name`) VALUES
 (1, 'BMW'),
 (2, 'Toyota'),
 (3, 'Opel'),
@@ -50,17 +52,19 @@ INSERT INTO `car_makers` (`car_maker_id`, `car_maker_name`) VALUES
 -- Table structure for table `car_types`
 --
 
-CREATE TABLE `car_types` (
-  `car_type_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `car_types`;
+CREATE TABLE IF NOT EXISTS `car_types` (
+  `car_type_id` int(11) NOT NULL AUTO_INCREMENT,
   `car_type` varchar(50) DEFAULT NULL,
-  `car_maker_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `car_maker_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`car_type_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `car_types`
 --
 
-INSERT INTO `car_types` (`car_type_id`, `car_type`, `car_maker_id`) VALUES
+INSERT DELAYED IGNORE INTO `car_types` (`car_type_id`, `car_type`, `car_maker_id`) VALUES
 (1, '3 Series', 1),
 (2, '5 Series', 1),
 (3, '7 Series', 1),
@@ -80,18 +84,20 @@ INSERT INTO `car_types` (`car_type_id`, `car_type`, `car_maker_id`) VALUES
 -- Table structure for table `images`
 --
 
-CREATE TABLE `images` (
-  `image_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `images`;
+CREATE TABLE IF NOT EXISTS `images` (
+  `image_id` int(11) NOT NULL AUTO_INCREMENT,
   `unique_item_id` varchar(10) DEFAULT NULL,
   `image_url` text DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`image_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `images`
 --
 
-INSERT INTO `images` (`image_id`, `unique_item_id`, `image_url`, `user_id`) VALUES
+INSERT DELAYED IGNORE INTO `images` (`image_id`, `unique_item_id`, `image_url`, `user_id`) VALUES
 (1, NULL, 'images/nature.JPG', 2),
 (23, 'qeLeEE', 'images/qeLeEE-vEzLiF_car1.jpg', 0),
 (24, 'qeLeEE', 'images/qeLeEE-YCm_fM_car2.jpg', 0),
@@ -117,7 +123,14 @@ INSERT INTO `images` (`image_id`, `unique_item_id`, `image_url`, `user_id`) VALU
 (44, 'kxEs2j', 'images/kxEs2j-KvilnG_car3.jpg', 0),
 (45, 'E1Dtu1', 'images/E1Dtu1-uJoOcX_car2.jpg', 0),
 (46, 'E1Dtu1', 'images/E1Dtu1-13NMXB_corsa1.jpg', 0),
-(47, 'E1Dtu1', 'images/E1Dtu1-I415TI_corsa2.jpg', 0);
+(47, 'E1Dtu1', 'images/E1Dtu1-I415TI_corsa2.jpg', 0),
+(48, '72Nq0M', 'images/72Nq0M-7g6bcK_astra2.jpg', 0),
+(49, 'FxLetd', 'images/FxLetd-cEVhN__golf1.jpg', 0),
+(50, 'FxLetd', 'images/FxLetd-YjqrR__golf2.jpg', 0),
+(51, 'FxLetd', 'images/FxLetd-qNiUtg_golf3.jpg', 0),
+(52, 'MA2eDS', 'images/MA2eDS-IJWSdS_corsa2.jpg', 0),
+(53, 'lEF375', 'images/lEF375-hE_mJX_e92  3.jpg', 0),
+(54, 'lEF375', 'images/lEF375-qNzJfo_e92 2.jpg', 0);
 
 -- --------------------------------------------------------
 
@@ -125,8 +138,9 @@ INSERT INTO `images` (`image_id`, `unique_item_id`, `image_url`, `user_id`) VALU
 -- Table structure for table `items`
 --
 
-CREATE TABLE `items` (
-  `item_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `items`;
+CREATE TABLE IF NOT EXISTS `items` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
   `unique_item_id` varchar(6) NOT NULL DEFAULT current_timestamp(),
   `item_title` varchar(50) DEFAULT NULL,
   `car_maker_id` int(11) DEFAULT NULL,
@@ -136,38 +150,28 @@ CREATE TABLE `items` (
   `item_price` float DEFAULT NULL,
   `item_thumbnail` text DEFAULT NULL,
   `item_date_posted` date NOT NULL,
-  `seller_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `seller_id` int(11) NOT NULL,
+  PRIMARY KEY (`item_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=96 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `items`
 --
 
-INSERT INTO `items` (`item_id`, `unique_item_id`, `item_title`, `car_maker_id`, `car_type_id`, `item_description`, `item_location`, `item_price`, `item_thumbnail`, `item_date_posted`, `seller_id`) VALUES
-(59, '_HaTag', 'test 1', 1, 2, 'test 1test 1test 1test 1test 1test 1test 1test 1test 1test 1test 1test 1test 1test 1test 1test 1test 1test 1test 1test 1test 1test 1test 1test 1test 1', 'Osijek', 132, 'images/_HaTag-car3.jpg', '2022-12-19', 0),
-(60, 'oQzfF5', 'Vw ', 2, 1, 'qweqwe wqe', 'qewqew', 12, 'images/oQzfF5-L6JROvm3EMS1wfevwpolo_2.jpg', '2022-12-19', 0),
-(61, 'G_Uzje', 'samo thumbnail', 3, 3, 'samo thumbnailsamo thumbnailsamo thumbnailsamo thumbnailsamo thumbnailsamo thumbnail', 'samo thumbnail', 1, 'images/G_Uzje-soyjack1233213.jpg', '2022-12-19', 0),
-(62, 'Eha5MZ', 'Test car 322', 0, 0, 'Testset lkestjlsketj l tjeslj tlesj tlsejt lsekjtlse tjlkj', 'Jankovci', 992, 'images/Eha5MZ-astra.jpg', '2022-12-30', 0),
-(64, 'kxEs2j', 'BMW 5 Series', 0, 0, 'ajkldlakdjsl', 'asda', 0, 'images/kxEs2j-bmw51.webp', '2022-12-30', 0),
-(67, 'fO5LsH', 'Valjd radi - Volkswa', 4, 10, 'qqqqqqqqqqq', 'Osijek', 1112, 'images/fO5LsH-tiguan.jpg', '2022-12-30', 0),
-(68, 'Gwwa--', 'BMW 3 Series F10', 1, 1, 'pali vozi', 'Vinkovci', 12411, 'images/Gwwa---f10.jpg', '2022-12-30', 0),
-(69, 'E1Dtu1', 'Opel Corsa', 3, 9, 'Corsa hehe Corsa hehe Corsa hehe Corsa hehe Corsa hehe Corsa hehe Corsa hehe Corsa hehe Corsa hehe Corsa hehe Corsa hehe Corsa hehe Corsa hehe Corsa hehe Corsa hehe Corsa hehe Corsa hehe Corsa hehe Corsa hehe Corsa hehe Corsa hehe Corsa hehe ', 'Osijek', 19998, 'images/E1Dtu1-corsa1.jpg', '2023-01-01', 0),
-(70, 'N0j5ey', 'Opel Insignia', 3, 7, '', '', 0, 'images/N0j5ey-astra.jpg', '2023-01-01', 0),
-(71, 'Rz6f43', 'BMW 7 Series', 1, 3, '', '', 0, 'images/Rz6f43-', '2023-01-01', 2),
-(72, 'dg6OrK', 'Toyota Yaris', 2, 6, '', '', 0, 'images/dg6OrK-', '2023-01-01', 2),
-(73, 'b7zPU2', 'rqrqwrqwrwq', 0, 0, '', '', 0, 'images/b7zPU2-', '2023-01-01', 1),
-(74, '7oq-0W', 'Opel Insignia', 3, 7, '', '', 0, 'images/7oq-0W-', '2023-01-01', 3);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `saved_items`
---
-
-CREATE TABLE `saved_items` (
-  `user_id` int(11) DEFAULT NULL,
-  `item_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT DELAYED IGNORE INTO `items` (`item_id`, `unique_item_id`, `item_title`, `car_maker_id`, `car_type_id`, `item_description`, `item_location`, `item_price`, `item_thumbnail`, `item_date_posted`, `seller_id`) VALUES
+(83, 'jLcQVn', 'BMW 3 Series - povoljno', 1, 1, 'test description bmw 3 seriestest description bmw 3 seriestest description bmw 3 seriestest description bmw 3 seriestest description bmw 3 series', 'Osijek', 19923, 'images/jLcQVn-e92 2.jpg', '2023-02-12', 14),
+(84, 'ggJsgq', 'BMW 3 Series - test', 1, 1, 'qqweeeeeqw', 'Vinkovci', 12331, 'images/ggJsgq-e92  3.jpg', '2023-02-12', 14),
+(85, 'qE73Wq', 'BMW 5 Series', 1, 2, 'qweqweeqwqwe', 'Osijek', 100000, 'images/qE73Wq-bmw51.jpg', '2023-02-12', 14),
+(86, 'tlYzpl', 'BMW 7 Series - 25k km', 1, 3, 'qweeqwqweqwe', 'Zagreb', 32293, 'images/tlYzpl-bmw7.jpg', '2023-02-12', 14),
+(87, 'sFiXta', 'Toyota Aigo - ostecen', 2, 4, 'test descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest description', 'Rijeka', 3000, 'images/sFiXta-aigo1.jpg', '2023-02-12', 14),
+(88, 'RNPQE6', 'Toyota Corolla', 2, 5, 'reliable', 'Otok', 12500, 'images/RNPQE6-corolla1.jpeg', '2023-02-12', 14),
+(89, 'I-nFgx', 'Toyota Yaris - TEST', 2, 6, 'qqqqqqqqqqqqqqqqqq q', 'Osijek', 5000, 'images/I-nFgx-yaris1.jpeg', '2023-02-12', 14),
+(90, 'pYzh1-', 'Opel Insignia', 3, 7, 'Test description', 'Osijek', 15000, 'images/pYzh1--insignia1.jpg', '2023-02-12', 15),
+(91, 'gU8h_M', 'Opel Astra OPC', 3, 8, 'OPC, 180k km', 'Vinkovci', 11000, 'images/gU8h_M-astra.jpg', '2023-02-12', 15),
+(92, 'xh5Kpb', 'Opel Corsa - TEST', 3, 9, 'qweqwqweqwe', 'Zagreb', 1700, 'images/xh5Kpb-corsa1.jpg', '2023-02-12', 15),
+(93, 'gLIx__', 'Volkswagen Tiguan TDI', 4, 10, 'Volkswagen Tiguan TDI descriptionVolkswagen Tiguan TDI descriptionVolkswagen Tiguan TDI description', 'Split', 18000, 'images/gLIx__-tiguan.jpg', '2023-02-12', 15),
+(94, 'X5BEb5', 'Volkswagen Passat TSI', 4, 11, 'passat 180kw chip tune', 'Osijek', 4400, 'images/X5BEb5-passat1.jpeg', '2023-02-12', 15),
+(95, '31dDC4', 'Volkswagen Golf GTI', 4, 12, 'edited opis - test', 'Osijek', 15000, 'images/31dDC4-golf2.jpg', '2023-02-12', 15);
 
 -- --------------------------------------------------------
 
@@ -175,92 +179,31 @@ CREATE TABLE `saved_items` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_email` varchar(50) DEFAULT NULL,
   `user_password` varchar(50) DEFAULT NULL,
   `username` varchar(50) DEFAULT NULL,
   `profile_picture_url` text DEFAULT NULL,
-  `user_phone` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `user_phone` varchar(10) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `user_email`, `user_password`, `username`, `profile_picture_url`, `user_phone`) VALUES
+INSERT DELAYED IGNORE INTO `users` (`user_id`, `user_email`, `user_password`, `username`, `profile_picture_url`, `user_phone`) VALUES
 (1, 'admin@admin', 'admin', 'admin', NULL, '4444444'),
 (2, 'stjepan@stjepan.com', 'stjepan', 'stjepan', 'images/nature.JPG', '099123123'),
 (9, 'q@q', 'q', 'q', 'images/2023-01-01_astra2.jpg_q', 'q'),
-(10, 'ee@ee', 'ee', 'ee', 'images/2023-01-01_ee_corsa1.jpg', '123');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `car_makers`
---
-ALTER TABLE `car_makers`
-  ADD PRIMARY KEY (`car_maker_id`);
-
---
--- Indexes for table `car_types`
---
-ALTER TABLE `car_types`
-  ADD PRIMARY KEY (`car_type_id`);
-
---
--- Indexes for table `images`
---
-ALTER TABLE `images`
-  ADD PRIMARY KEY (`image_id`);
-
---
--- Indexes for table `items`
---
-ALTER TABLE `items`
-  ADD PRIMARY KEY (`item_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `car_makers`
---
-ALTER TABLE `car_makers`
-  MODIFY `car_maker_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `car_types`
---
-ALTER TABLE `car_types`
-  MODIFY `car_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT for table `images`
---
-ALTER TABLE `images`
-  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
-
---
--- AUTO_INCREMENT for table `items`
---
-ALTER TABLE `items`
-  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+(10, 'ee@ee', 'ee', 'ee', 'images/2023-01-01_ee_corsa1.jpg', '123'),
+(11, 'bruh@bruh', 'bruh', 'bruh', 'images/2023-01-02_bruh_bobby.jpg', '991293'),
+(12, 'prof@prof.com', 'prof', 'prof', 'images/2023-02-12_prof_f10.jpg', '0999999999'),
+(13, 'prof@prof.com', 'prof', 'prof', 'images/2023-02-12_prof_f10.jpg', '0999999999'),
+(14, 'stjepan@stjepan', 'stjepan', 'stjepan', 'images/2023-02-12_stjepan_e92.jpg', '099123123'),
+(15, 'test@test', 'test', 'test', 'images/2023-02-12_test_astra.jpg', '123123');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
